0f9e8be0125c50f74b2e8336fafe81d3d76c50c7
Rowland Carlson <rowland@stillclever.com>
2020-04-23 19:37:37 -0500

    added update test

--- test.rb
+++ test.rb
@@ -30,4 +30,10 @@ class SqlTest < Minitest::Test
 	        assert_equal(res[0]['email'], "test@email.com")	
 	end
 
+	def test_new_primary
+		DB.exec("UPDATE contacts.email_address SET primary_email = False WHERE email = 'rowland@stillclever.com';")
+		DB.exec("UPDATE contacts.email_address SET primary_email = True WHERE email = 'test@gmail.com';")
+		res = DB.exec("SELECT * FROM contacts.email_address WHERE email = 'test@gmail.com';")
+		assert_equal(res[0]['primary_email'], 't')
+	end
 end
